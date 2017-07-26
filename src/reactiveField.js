45461c734a7b71a073069d82245ffd1b7ff3e1cd
Max Nowack <maxnowack@users.noreply.github.com>
2017-07-26 10:37:09 +0200

    expose cache object on reactiveField

--- src/reactiveField.js
+++ src/reactiveField.js
@@ -2,5 +2,7 @@ import DataCache from './DataCache';
 
 export default (fn, compare) => {
   const cache = new DataCache(key => fn(...JSON.parse(key)), compare);
-  return (...args) => cache.get(JSON.stringify(args));
+  const resolver = (...args) => cache.get(JSON.stringify(args));
+  resolver.cache = cache;
+  return resolver;
 };
